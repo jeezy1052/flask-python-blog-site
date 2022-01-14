bd807d729316b3f48666226180bc0abca8e38d5f
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-01-13 18:52:24 -0800

    i have just add the post functions

--- website/auth.py
+++ website/auth.py
@@ -23,7 +23,8 @@ def login():
                 flash("Password is incorrect.", category="error") 
         else:
             flash("Email does not exist.", category="error")
-    return render_template("login.html")
+
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/sign-up", methods=["GET", "POST"])
@@ -57,7 +58,7 @@ def sign_up():
             flash("User created!")
             return redirect(url_for("views.home"))
 
-    return render_template('signup.html')
+    return render_template('signup.html', user=current_user)
 
 
 @auth.route("/logout")
--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import backref
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -8,3 +9,10 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    post = db.relationship("Post", backref="user", passive_deletes=True)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
